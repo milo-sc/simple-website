cadae0ae281b0033b617df43b4800679046999dc
Victor Wu <vicwu.off@gmail.com>
2025-01-17 19:27:53 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Heading from './components/Heading';
+
+function App() {
+  return (
+    <div className='App'>
+      <Heading />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+      <div className="copyright">
+          <p>Made with love by Myself</p>
+      </div>
+  );
+};
+
+export default Footer;
