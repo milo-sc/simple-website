d5394940224c49ab040ccfc89f1fe76caca6c89d
Victor Wu <vicwu.off@gmail.com>
2025-01-22 21:36:37 -0500

    adds css styling to Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Intro1 from './components/Intro1';
