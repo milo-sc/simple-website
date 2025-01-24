6e2016faa549742216c992d4d9ea751a89e166f0
Victor Wu <vicwu.off@gmail.com>
2025-01-23 20:40:29 -0500

    adds sizing for promo picture

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Promo.css';
 
 import carlotta from '../images/carlotta.jpeg';
 
 function Promo() {
   return (
     <div>
-        <img src={carlotta} alt="Carlotta"></img>
+        <img className='promo' src={carlotta} alt="Carlotta"></img>
     </div>
     );
 };
