d1d78c75c797f6c88ad176ed609d845450b24c37
Victor Wu <vicwu.off@gmail.com>
2025-01-29 20:17:36 -0500

    adds about page and more information to the footer, might rearrange footer later

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,12 @@ import './Footer.css';
 function Footer() {
   return (
       <div className="footer">
-          <p>228 40th Street</p>
-          <p>Brooklyn, NY 11232</p>
+        <p>Elegance Manufacturing, co.</p>
+        <p>228 40th Street</p>
+        <p>Brooklyn, NY 11232</p>
+
+        <p>Telephone: (718)923-6087</p>
+        <p>Fax: (718)923-6092</p>
       </div>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,15 @@ import './About.css';
 function About() {
   return (
     <div className='about'>
-        <h1>About Elegance Manufacturing</h1>
+      <p>
+        Every kind booth and table matched in all kinds of directions, including shapes, colors, materials, and designs.
+        Many different choices are provided such as circular booth, single side booth, double side booth, long bench, the L-shaped, horizontal and vertical channel, headrool, OAK wood trimming, Formica trimming, the Formica frog-back type, OAK wood work or stainless steel button, anticipating to math with different skins and materials.
+        Varieties of table tops also includes OAK wood side, plastic edge, Formica edge, electroplate edge, as well as different paints.
+        The Formica also can be provided with wood grain with over hundreds of different colors and patterns.
+        Our company also provides over hundreds of styles, such as the American style, Chinese style, Vietnamese style, Japanese style, European style as well as five-in-one table or chairs, completed with combinations of different variety of chairs.
+
+        In order to gratify each and every one of our customers, we assure you the most reasonable price with the flexibility of choosing whatever design you have in mind, fulfilling your wishes as well as we can.
+      </p>
     </div>
     );
 };
