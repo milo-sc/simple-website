58023f5ced0692053b71384a9fe364493df650d6
Victor Wu <vicwu.off@gmail.com>
2025-02-21 00:34:52 -0500

    adds navlinkimage

---
+++ src/components/NavLinkImage.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './NavLinkUnderlined';
+
+function NavLinkUnderlined() {
+  return (
+    <div className='navLinkUnderlined'>
+
+    </div>
+  )
+}
+
+export default NavLinkUnderlined;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import './Navigation.css';
 
-import UnderlinedNavLink from './UnderlinedNavLink';
+import NavLinkUnderlined from './NavLinkUnderlined';
 
 import { useLanguage } from '../contexts/Language';
 
@@ -12,21 +12,21 @@ function Navigation(props: { isHovered: boolean; }) {
     <div>
       <nav>
         <ul className='navBar'>
-          <li><UnderlinedNavLink to="/booths" isHovered = {props.isHovered}>
+          <li><NavLinkUnderlined to="/booths" isHovered = {props.isHovered}>
                 {isEnglish ? 'Booths' : '卡座沙发'}
-              </UnderlinedNavLink></li>
+              </NavLinkUnderlined></li>
 
-          <li><UnderlinedNavLink to="/chairs" isHovered = {props.isHovered}>
+          <li><NavLinkUnderlined to="/chairs" isHovered = {props.isHovered}>
                 {isEnglish ? 'Chairs' : '椅子'}
-              </UnderlinedNavLink></li>
+              </NavLinkUnderlined></li>
 
-          <li><UnderlinedNavLink to="/tables" isHovered = {props.isHovered}>
+          <li><NavLinkUnderlined to="/tables" isHovered = {props.isHovered}>
                 {isEnglish ? 'Tables' : '桌子'}
-              </UnderlinedNavLink></li>
+              </NavLinkUnderlined></li>
               
-          <li><UnderlinedNavLink to="/special" isHovered = {props.isHovered}>
+          <li><NavLinkUnderlined to="/special" isHovered = {props.isHovered}>
                 {isEnglish ? 'Special' : '其它'}
-              </UnderlinedNavLink></li>
+              </NavLinkUnderlined></li>
         </ul>
       </nav>
     </div>
