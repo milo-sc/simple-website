35b982d66c2214658832a978a3d3a4c4fabeb1c3
Victor Wu <vicwu.off@gmail.com>
2025-03-11 20:40:42 -0400

    adds background video component

---
+++ src/components/BackgroundVideo.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './BackgroundVideo.css'
+
+function BackgroundVideo() {
+  return (
+    <div className='blurb'>
+      
+    </div>
+  )
+}
+
+export default BackgroundVideo;
