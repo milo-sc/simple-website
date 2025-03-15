8983a877d8c7ad9df9bf70cfe3f100f56529b55e
Victor Wu <vicwu.off@gmail.com>
2025-03-15 14:36:39 -0400

    whys this background so hard to fix

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -4,15 +4,22 @@ import './BackgroundVideo.css'
 function BackgroundVideo() {
   return (
     <div>
-      <video 
-          poster='/home_images/brownTableSofas.jpeg' 
-          autoPlay 
-          muted 
-          loop 
-          playsInline 
-          className='backgroundVideo'>
-          <source src="/background.mp4" type="video/mp4"></source>
-        </video>
+      <video
+        autoPlay 
+        muted 
+        loop 
+        playsInline
+        className='backgroundVideo'>
+        <source src="/background.mp4" type="video/mp4"></source>
+      </video>
+
+      {/* {hasVideoError && (
+        <img
+          src='/home_images/brownTableSofas.jpeg'
+          alt='backup'
+          className='backupImage'
+        />
+      )} */}
     </div>
   )
 }
